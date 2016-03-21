a34b6690eb6dcf16839570deccdb1bf2be7d1b35
Student <Student@Students-Mac-mini-2.local>
2016-03-21 14:22:24 -0600

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+// PONG COURT
 var pongCourt = document.getElementById ('canvas');
 var context = pongCourt.getContext('2d');
-context.fillstyle='yellow';
+context.fillStyle='purple';
 context.fillRect(0, 0, 1000, 1000);
+
+// CENTER DIVIDER
+context.fillStyle='#95E3EC';
+context.fillRect(365, 0, 12, 400);
+
+
+// LEFT PONG PADDLE
+context.fillStyle='#95E3EC';
+context.fillRect(10, 160, 10, 50);
+
+// RIGHT PONG PADDLE
+context.fillStyle='#95E3EC';
+context.fillRect(730, 185, 10, 50);
